1f83087667b5d5bcae6dfa759abe1711eec75f59
Rasmus Précenth <rasmus@precenth.eu>
2021-12-01 12:56:08 +0100

    Update to 21.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     nixpkgs-old.url = "github:NixOS/nixpkgs/38fce8ec004b3e61c241e3b64c683f719644f350";
-    home-manager.url = "github:nix-community/home-manager/release-21.05";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs-review.url = "github:Mic92/nixpkgs-review";
     nixpkgs.inputs.nixpkgs.follows = "nixpkgs";
