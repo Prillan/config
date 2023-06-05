9fae2dc0e522074d262d89689bdb226f0e9ba002
Rasmus Précenth <rasmus@precenth.eu>
2023-06-05 20:11:54 +0200

    {nixpkgs,home-manager}: 22.11 -> 23.05

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs-review.url = "github:Mic92/nixpkgs-review";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
