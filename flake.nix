60b7171aa4e319fd89523235928a7a15dc9103f1
Rasmus Précenth <rasmus@precenth.eu>
2025-06-27 19:33:09 +0200

    refactor flake.nix using flake-parts

--- flake.nix
+++ flake.nix
@@ -15,162 +15,152 @@
     emacs-overlay.url = "github:nix-community/emacs-overlay";
 
     flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, unstable, flake-utils, emacs-overlay, home-manager, nixpkgs-review }: {
-    overlays = {
-      default = final: prev:
-        let pkgs = unstable.legacyPackages.${prev.system};
-        in {
-          inherit (pkgs) josm nix-zsh-completions;
+  outputs = inputs@{ self, flake-parts, emacs-overlay, nixpkgs-review, flake-utils, unstable, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({withSystem, config, ...}: {
+      systems = [ "x86_64-linux" ];
+      imports = [
+        inputs.home-manager.flakeModules.home-manager
+      ];
+      flake = {
+        overlays = {
+          default = final: prev:
+            let pkgs = unstable.legacyPackages.${prev.system};
+            in {
+              inherit (pkgs) josm nix-zsh-completions;
+              nixpkgs-review = nixpkgs-review.packages.${prev.system}.default;
+            };
         };
-    };
-    lib = {
-      homeConfigurationBase = { system ? "x86_64-linux" }: {
-        imports = [
-          ./nixpkgs/home.nix
-        ];
-        nixpkgs.overlays = [
-          emacs-overlay.overlay
-          self.overlays.default
-        ];
-        home.packages = [ nixpkgs-review.packages.${system}.default ];
 
-        nix.registry =
-          let
-            reg = { id, input, type, owner, repo }: {
-              from = {
-                inherit id;
-                type = "indirect";
-              };
-              to = {
-                inherit (input) rev narHash lastModified;
-                inherit type owner repo;
-              };
-            };
-          in
-            {
-              nixpkgs = reg {
-                id = "nixpkgs";
-                input = self.inputs.nixpkgs;
-                type = "github";
-                owner = "NixOS";
-                repo = "nixpkgs";
-              };
-              unstable = reg {
-                id = "unstable";
-                input = self.inputs.unstable;
-                type = "github";
-                owner = "NixOS";
-                repo = "nixpkgs";
-              };
-              config = {
-                from = {
-                  id = "config";
-                  type = "indirect";
+        homeModules = {
+          base = {
+            imports = [
+              ./nixpkgs/home.nix
+            ];
+            nixpkgs.overlays = [
+              emacs-overlay.overlay
+              self.overlays.default
+            ];
+
+            nix.registry =
+              let
+                reg = { id, input, type, owner, repo }: {
+                  from = {
+                    inherit id;
+                    type = "indirect";
+                  };
+                  to = {
+                    inherit (input) rev narHash lastModified;
+                    inherit type owner repo;
+                  };
                 };
-                to = {
-                  type = "github";
-                  owner = "Prillan";
-                  repo = "config";
+              in
+                {
+                  nixpkgs = reg {
+                    id = "nixpkgs";
+                    input = inputs.nixpkgs;
+                    type = "github";
+                    owner = "NixOS";
+                    repo = "nixpkgs";
+                  };
+                  unstable = reg {
+                    id = "unstable";
+                    input = inputs.unstable;
+                    type = "github";
+                    owner = "NixOS";
+                    repo = "nixpkgs";
+                  };
+                  config = {
+                    from = {
+                      id = "config";
+                      type = "indirect";
+                    };
+                    to = {
+                      type = "github";
+                      owner = "Prillan";
+                      repo = "config";
+                    };
+                  };
                 };
-              };
-            };
-      };
-      homeConfiguration = { configuration, username ? "rasmus", system ? "x86_64-linux", extraModules ? [ ] }:
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${system};
-          modules = [
-            (self.lib.homeConfigurationBase { inherit system; })
-            configuration
-            {
-              home = {
-                username = username;
-                homeDirectory = "/home/${username}";
-              };
+          };
+
+          rasmus-graphical-user = {
+            imports = [ self.homeModules.base ];
+            profiles.fluff.enable = true;
+            profiles.graphical.wayland.enable = true;
+            profiles.mapping.enable = true;
+            borg.enable = true;
+            programs.home-manager.enable = true;
+          };
+        };
+
+        homeConfigurations = {
+          "rasmus@kalmiya" = withSystem "x86_64-linux" (
+            {pkgs, ...}:
+            inputs.home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+
+              modules = [
+                self.homeModules.rasmus-graphical-user
+                {
+                  home = {
+                    username = "rasmus";
+                    homeDirectory = "/home/rasmus";
+                  };
+                  custom.hostname = "kalmiya";
+                  custom.wifiInterface = "wlp2s0";
+                }
+              ];
             }
-          ] ++ extraModules;
+          );
         };
-      homeConfigurations = {
-        "rasmus@kalmiya" = { system }: { ... }: {
-          imports = [
-            (self.lib.homeConfigurationBase { inherit system; })
-          ];
-          custom.hostname = "kalmiya";
-          custom.wifiInterface = "wlp2s0";
-          profiles.fluff.enable = true;
-          profiles.graphical.wayland.enable = true;
-          profiles.mapping.enable = true;
-          borg.enable = true;
+
+        nixosModules.rsync-backup = import ./nixos/modules/backup.nix;
+
+        templates.shell = {
+          path = ./templates/shell;
+          description = "A quick ad-hoc-ish shell";
         };
       };
-    };
 
-    nixosModules.rsync-backup = import ./nixos/modules/backup.nix;
+      perSystem = { pkgs, ...}: {
+        apps.copy-theme = {
+          type = "app";
+          program =
+            let
+              prog = pkgs.writeShellApplication {
+                name = "copy-theme";
+                text = ''
+                  mkdir -p wal
+                  if [[ -v 1 ]]; then
+                    pushd "$1"
+                    TARGET="$(pwd)"
+                    popd
+                  else
+                    TARGET=$(pwd)/wal
+                  fi
+                  pushd ~/.cache/wal
 
-    templates.shell = {
-      path = ./templates/shell;
-      description = "A quick ad-hoc-ish shell";
-    };
-  } // flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: {
-    apps = {
-      # First run "wal ...", then "nix run .#copy-theme".
-      copy-theme = {
-        type = "app";
-        program =
-          let
-            prog = nixpkgs.legacyPackages.${system}.writeShellApplication {
-              name = "copy-theme";
-              text = ''
-                mkdir -p wal
-                if [[ -v 1 ]]; then
-                  pushd "$1"
-                  TARGET="$(pwd)"
+                  cp colors{.json,.hs,.Xresources,-kitty.conf,-sway,-waybar.css} \
+                     colors-wal-dmenu.h \
+                     "$TARGET"
+
                   popd
-                else
-                  TARGET=$(pwd)/wal
-                fi
-                pushd ~/.cache/wal
 
-                cp colors{.json,.hs,.Xresources,-kitty.conf,-sway,-waybar.css} \
-                   colors-wal-dmenu.h \
-                   "$TARGET"
+                  pushd "$TARGET"
 
-                popd
+                  sed -i 's/"wallpaper": ".*"/"wallpaper": "None"/' colors.json
+                  sed -i 's/wallpaper=".*"/wallpaper="None"/' colors.hs
+                  sed -i '/set .wallpaper/d' colors-sway
 
-                pushd "$TARGET"
-
-                sed -i 's/"wallpaper": ".*"/"wallpaper": "None"/' colors.json
-                sed -i 's/wallpaper=".*"/wallpaper="None"/' colors.hs
-                sed -i '/set .wallpaper/d' colors-sway
-
-                popd
-              '';
-            };
-          in
-          "${prog}/bin/copy-theme";
-      };
-    };
-
-    checks = {
-      # x11 = (self.lib.homeConfiguration {
-      #   inherit system;
-      #   configuration = {
-      #     profiles.graphical.x11.enable = true;
-      #     custom.hostname = "check";
-      #     custom.wifiInterface = "wl-test";
-      #   };
-      # }).activationPackage;
-
-      wayland = (self.lib.homeConfiguration {
-        inherit system;
-        configuration = {
-          profiles.graphical.wayland.enable = true;
-          custom.hostname = "check";
-          custom.wifiInterface = "wl-test";
+                  popd
+                '';
+              };
+            in
+              "${prog}/bin/copy-theme";
         };
-      }).activationPackage;
-    };
-  }
-  );
+      };
+    });
 }
