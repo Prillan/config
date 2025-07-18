dea41d1a65ae3a9540af31221156c28a1c922c99
Rasmus Précenth <rasmus@precenth.eu>
2025-07-18 13:33:50 +0200

    fix deprecation warning

--- home/base.nix
+++ home/base.nix
@@ -149,7 +149,7 @@ in {
         plugins = [ "autojump" ];
         custom = zsh-custom-path;
       };
-      initExtra = ''
+      initContent = ''
         unset SSH_AGENT_PID
         if [ "''${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
             export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
